62d23461f2b1c27c9d13b37749f6465158798dfa
Bagas Raditya Nafi <bagasrnfull@gmail.com>
2022-07-16 17:25:48 +0700

    Feat : Add Models

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Book;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -9,6 +10,8 @@ class BookController extends Controller
 {
     public function index()
     {
-        return Inertia::render('LandingPage');
+        return Inertia::render('LandingPage', [
+            'books' => Book::all()
+        ]);
     }
 }
